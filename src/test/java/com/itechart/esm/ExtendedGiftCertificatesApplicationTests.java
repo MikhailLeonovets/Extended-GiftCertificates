0d77155a2980a318e7a16105a30e77145941d04a
Mikhail Leonovets <mikhail.leonovets@gmail.com>
2022-04-15 12:28:45 +0300

    first commit with base of spring boot gradle project

---
+++ src/test/java/com/itechart/esm/ExtendedGiftCertificatesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itechart.esm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExtendedGiftCertificatesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
