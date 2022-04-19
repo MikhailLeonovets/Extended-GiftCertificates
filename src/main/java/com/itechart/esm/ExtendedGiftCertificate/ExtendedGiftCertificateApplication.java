3832f16887aabbe0ec745ced107c55c061bf2016
Mikhail Leonovets <mikhail.leonovets@gmail.com>
2022-04-19 14:07:56 +0300

    gradle files created

---
+++ src/main/java/com/itechart/esm/ExtendedGiftCertificate/ExtendedGiftCertificateApplication.java
@@ -0,0 +1,13 @@
+package com.itechart.esm.ExtendedGiftCertificate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtendedGiftCertificateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExtendedGiftCertificateApplication.class, args);
+	}
+
+}
