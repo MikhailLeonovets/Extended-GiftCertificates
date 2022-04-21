d7526f4c19d93ec36caf2ff9f8c827b7a51f6562
Mikhail Leonovets <mikhail.leonovets@gmail.com>
2022-04-21 17:03:53 +0300

    UserGiftCertificateNotFoundException created UserNotFoundException created UserService.java created UserServiceImpl created

---
+++ service/src/main/java/com/itechart/esm/service/UserService.java
@@ -0,0 +1,25 @@
+package com.itechart.esm.service;
+
+import com.itechart.esm.common.model.entity.User;
+import com.itechart.esm.service.exception.DataInputException;
+import com.itechart.esm.service.exception.UserNotFoundException;
+
+import java.util.List;
+
+public interface UserService {
+
+	User save(User user) throws DataInputException;
+
+	List<User> findAll();
+
+	User findById(Long id) throws UserNotFoundException, DataInputException;
+
+	User findByLogin(String login) throws UserNotFoundException, DataInputException;
+
+	boolean update(User user) throws DataInputException, UserNotFoundException;
+
+	boolean delete(User user) throws UserNotFoundException, DataInputException;
+
+	boolean deleteById(Long id) throws DataInputException, UserNotFoundException;
+
+}
