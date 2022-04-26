2e941c54d0ae806077c083f623f7c948bb295097
Mikhail Leonovets <mikhail.leonovets@gmail.com>
2022-04-26 17:58:42 +0300

    UrlStorage updated

--- controller/src/main/java/com/itechart/esm/controller/security/storage/UrlStorage.java
+++ controller/src/main/java/com/itechart/esm/controller/security/storage/UrlStorage.java
@@ -1,7 +1,7 @@
 package com.itechart.esm.controller.security.storage;
 
 public class UrlStorage {
-	public static final String SIGN_IN_POST_MAPPING = "/gift-certificate/sign-in";
+	public static final String SIGN_IN_POST_MAPPING = "/gift-certificate/login";
 	public static final String SIGN_UP_POST_MAPPING = "/gift-certificate/sign-up";
 
 }
